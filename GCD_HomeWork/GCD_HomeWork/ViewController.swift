974f2ec77b9236df479aaf06abdcb072b7cea133
Bach <motirelia612@gmail.com>
2023-08-30 08:40:15 +0700

    Initialize Projet

---
+++ GCD_HomeWork/GCD_HomeWork/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GCD_HomeWork
+//
+//  Created by Bach Nghiem on 30/08/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
