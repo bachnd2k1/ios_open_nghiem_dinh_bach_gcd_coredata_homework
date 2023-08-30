05b820df429c9c78e83f2f485a168cfc8cc91a91
Bach <motirelia612@gmail.com>
2023-08-30 08:40:15 +0700

    Initialize Projet

---
+++ GCD_HomeWork/GCD_HomeWork/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  GCD_HomeWork
+//
+//  Created by Bach Nghiem on 30/08/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ GCD_HomeWork/GCD_HomeWork/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  GCD_HomeWork
+//
+//  Created by Bach Nghiem on 30/08/2023.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
